cf072289fa81dc92d31815856629d70a72284a16
Niklas Schmelzle <niklas.schmelzle@gmail.com>
2021-03-12 19:04:52 +0100

    Working encryption via uart (but buggy)
    Able to send plaintext and key via uart. FPGA processes ciphertext and
    sends it back via uart. Unfortunately it is still quite buggy.

--- hdl/top.vhd
+++ hdl/top.vhd
@@ -12,8 +12,6 @@ end entity;
 
 
 architecture top_arch of top is
-	type top_state is (INIT, RESET, CNT, ENCPT, ENCPT_W1, ENCPT_W2, ENCPT_W3, TRGLED);
-	signal state : top_state := INIT;
 
 	component aes_encrypt is
 		port (
@@ -25,12 +23,10 @@ architecture top_arch of top is
 			ciphertext : out std_logic_vector(127 downto 0)
 		);
 	end component;
-	signal new_data, valid : std_logic := '0';
+	signal new_data, valid, valid_d, valid_w : std_logic := '0';
 	signal plaintext, key, ciphertext : std_logic_vector(127 downto 0) := (others => '0');
 
 	signal s_led : std_logic := '0';
-
-	signal counter : integer range 0 to 16000001 := 0;
 
 
 	constant clk_divider : integer := 104;
@@ -52,6 +48,14 @@ architecture top_arch of top is
 	signal tbyte, rbyte : std_logic_vector(7 downto 0) := (others => '0');
 	signal tbyte_f, rbyte_f : std_logic := '0';
 
+	signal input_buf : std_logic_vector(287 downto 0) := (others => '0');
+	signal eot, eot_d : std_logic := '0';
+
+	type tx_state_t is (IDLE, TX_WRT, TX_W);
+	signal tx_state : tx_state_t := IDLE;
+	signal tx_byte_cnt : integer := 0;
+	signal tx_wait_cnt : integer range 0 to 100 := 0;
+
 begin
 	-- generate from 16 MHz clk 19200*8 Hz clk
 	clk_dvd_proc : process (clk) is
@@ -68,69 +72,111 @@ begin
 			end if;
 		end if;
 	end process;
+
 	rx <= PIN_1;
 	PIN_2 <= tx;
-	tbyte_f <= rbyte_f;
-	tbyte <= rbyte;
-	uart1 : uart port map (uart_clk, tx, rx, tbyte, tbyte_f, rbyte, rbyte_f);
-
-
 	LED <= s_led;
+
+	uart1 : uart port map (uart_clk, tx, rx, tbyte, tbyte_f, rbyte, rbyte_f);
 	aes_encrypt_1 : aes_encrypt port map (CLK, new_data, plaintext, key, valid, ciphertext);
 
-	nstate_proc : process (CLK) is
+
+	led_proc : process (clk) is
 	begin
-		if rising_edge(CLK) then
-			case state is
-				when INIT =>
-					state <= RESET;
-				when RESET =>
-					state <= CNT;
-				when CNT =>
-					if counter = 16000000 then
-						state <= ENCPT;
+		if rising_edge(clk) then
+			valid_d <= valid;
+			if valid_d = '0' and valid = '1' then
+				s_led <= not s_led;
+			end if;
+		end if;
+	end process;
+
+
+	buf_proc : process (uart_clk) is
+	begin
+		if rising_edge(uart_clk) then
+			if rbyte_f = '1' then
+				input_buf <= input_buf(279 downto 0) & rbyte;
+			end if;
+		end if;
+	end process;
+
+	eot_proc : process(clk) is
+	begin
+		if rising_edge(clk) then
+			eot_d <= eot;
+			if input_buf(31 downto 0) = x"000000FF" then
+				eot <= '1';
+			else
+				eot <= '0';
+			end if;
+		end if;
+	end process;
+
+	trg_encpt_proc : process(clk) is
+	begin
+		if rising_edge(clk) then
+			new_data <= '0';
+			if eot_d = '0' and eot = '1' then
+				new_data <= '1';
+				plaintext <= input_buf(287 downto 160);
+				key <= input_buf(159 downto 32);
+			end if;
+		end if;
+	end process;
+
+	valid_w_proc : process (clk) is
+	begin
+		if rising_edge(clk) then
+			if valid_d = '0' and valid = '1' then
+				valid_w <= '1';
+			elsif tx = '0' then
+				valid_w <= '0';
+			end if;
+		end if;
+	end process;
+
+	nstate_tx_proc : process (uart_clk) is
+	begin
+		if rising_edge(uart_clk) then
+			case tx_state is
+				when IDLE =>
+					if valid_w = '1' then
+						tx_state <= TX_WRT;
 					end if;
-				when ENCPT =>
-					state <= ENCPT_W1;
-				when ENCPT_W1 =>
-					state <= ENCPT_W2;
-				when ENCPT_W2 =>
-					state <= ENCPT_W3;
-				when ENCPT_W3 =>
-					if valid = '1' then
-						state <= TRGLED;
+				when TX_WRT =>
+					tx_state <= TX_W;
+				when TX_W =>
+					if tx_wait_cnt = 82 then
+						tx_state <= TX_WRT;
+						if tx_byte_cnt = 16 then
+							tx_state <= IDLE;
+						end if;
 					end if;
-				when TRGLED =>
-					state <= RESET;
 			end case;
 		end if;
 	end process;
 
-	out_proc : process (CLK) is
+	out_tx_proc : process (uart_clk) is
+		variable bit_pos : integer := 0;
 	begin
-		if rising_edge(CLK) then
-			case state is
-				when INIT =>
-					s_led <= '0';
-					new_data <= '0';
-					plaintext <= (others => '0');
-					key <= (others => '0');
-				when RESET =>
-					counter <= 0;
-				when CNT =>
-					counter <= counter + 1;
-				when ENCPT =>
-					plaintext <= x"4F816B7C87A0563D0D84BDE984A33D03";
-					key <= x"9D5BFF851B0B81F841E7196736524BBD";
-					new_data <= '1';
-				when ENCPT_W1 =>
-					new_data <= '0';
-				when TRGLED =>
-					if ciphertext = x"BACF80FA05DF776E90CBF0E7D13335B4" then
-						s_led <= not s_led;
-					end if;
-				when others =>
+		if rising_edge(uart_clk) then
+			case tx_state is
+				when IDLE =>
+					tx_byte_cnt <= 0;
+				when TX_WRT =>
+					tx_byte_cnt <= tx_byte_cnt + 1;
+					tx_wait_cnt <= 0;
+					for i in 0 to 7 loop
+						bit_pos := i + 120 - 8 * tx_byte_cnt;
+						tbyte(i) <= ciphertext(bit_pos);
+					end loop;
+					tbyte_f <= '1';
+				when TX_W =>
+					tx_wait_cnt <= tx_wait_cnt + 1;
+					tbyte_f <= '0';
 			end case;
 		end if;
 	end process;
+
 end architecture;
